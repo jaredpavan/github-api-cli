5accb6a382d886e8a861e3e0600a8bb4fd07af6e
jared-pavan <jared.pavan@opower.com>
2015-06-09 17:21:45 -0400

    adding pretty print

--- github-api-cli.sh
+++ github-api-cli.sh
@@ -9,13 +9,13 @@ function isDivisibleBy {
   # Get count of issues
   all_issues_count=$( echo $all_issues | jq 'length - 1' )
   # Create list
-  issue_comments_title="Comment Count'\t'Issue Title'\n\r'"
+  issue_comments_title=$(echo -e "Comments\tIssue Title\n\r")
   for idx in $(seq 0 $all_issues_count); do
     issue_comments_title="$issue_comments_title$(echo -e '\n\r')"
     issue_comments_title="$issue_comments_title$( echo -e $( echo $all_issues | jq .[$idx].comments )'\t'$( echo $all_issues | jq .[$idx].title ))"
   done
 
-  echo "$issue_comments_title" | sort -r | head -n $item_count
+  echo "$issue_comments_title" | sort -r | head -n $item_count | column -t -s $'\t'
 }
 
 isDivisibleBy
